20c509deea57477e550b4509a81de9b7008b34f4
danildany <76399818+danildany@users.noreply.github.com>
2021-08-10 09:08:43 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app=express();
+const pinRoute = require('./routes/pins')
+const userRoute = require('./routes/users')
+dotenv.config();
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,useUnifiedTopology: true })
+.then(()=>{console.log('Mongodb conected')})
+.catch(e=> console.log(e));
+
+app.use('/api/pins',pinRoute);
+app.use('/api/users',userRoute);
+
+app.listen(8800,()=>{
+    console.log('backend server is running');
+})
