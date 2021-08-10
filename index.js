73fe1414abdacbcd999b4c6aaa2ce01166cdbd4c
danildany <76399818+danildany@users.noreply.github.com>
2021-08-10 11:16:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const pinRoute = require('./routes/pins')
 const userRoute = require('./routes/users')
 dotenv.config();
 app.use(express.json());
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next(); };  
+app.use(allowCrossDomain);
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,useUnifiedTopology: true })
 .then(()=>{console.log('Mongodb conected')})
 .catch(e=> console.log(e));
